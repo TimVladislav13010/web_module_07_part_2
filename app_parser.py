6e64aa2280e8091ec00753c96dfe750244d9e140
Vladislav <109065019+TimVladislav13010@users.noreply.github.com>
2023-03-06 17:46:01 +0200

    add create, update

--- app_parser.py
+++ app_parser.py
@@ -12,18 +12,18 @@ py main.py -a create -m Group -n 'AD-101'
 def arg_parser():
     parser = argparse.ArgumentParser(description='A console utility for CRUD operations with a db.')
 
-    parser.add_argument('-a', '--action', type=str, required=True)
-    parser.add_argument('-m', '--model', type=str, required=True)
-    parser.add_argument('-n', '--name', type=str)
-    parser.add_argument('-i', '--id', type=int)
+    parser.add_argument('-a', '--action', choices=["create", "list", "update", "remove"], type=str, required=True)
+    parser.add_argument('-m', '--model', choices=["Group", "Student", "Teacher", "Discipline", "Grade"], type=str, required=True)
+    parser.add_argument('-n', '--name', type=str, default=None)
+    parser.add_argument('-i', '--id', type=int, default=None)
+    parser.add_argument('-gi', '--group_id', type=int, default=None)
+    parser.add_argument('-ti', '--teacher_id', type=int, default=None)
+    parser.add_argument('-di', '--discipline_id', type=int, default=None)
+    parser.add_argument('-si', '--student_id', type=int, default=None)
+    parser.add_argument('-fn', '--full_name', type=str, default=None)
+    parser.add_argument('-g', '--grade', type=int, default=None)
+    parser.add_argument('-d', '--date_of', type=str, default=None)
 
-    args = vars(parser.parse_args())  # object -> dict
-    command_pars = args.get('action').lower()
-    column_pars = args.get('model').lower()
-    value_pars = args.get('name')
-    id_pars = args.get('id')
+    args = parser.parse_args()  # object -> dict
 
-    return {"command": command_pars,
-            "column": column_pars,
-            "value": value_pars,
-            "id": id_pars}
+    return args
--- main.py
+++ main.py
@@ -1,24 +1,143 @@
+from datetime import datetime
+
 from app_parser import arg_parser
-from crud_commands import crud_commands
-
-
-def handler_commands():
-    arguments = arg_parser()
-    commands = crud_commands()
-    print(arguments)
-    for command in commands.keys():
-        if arguments.get("command") in command and len(arguments) == 3:
-            return commands.get(command)(arguments.get("column"), arguments.get("value"))
-        elif arguments.get("command") in command and len(arguments) == 2:
-            return commands.get(command)(arguments.get("column"))
-        elif arguments.get("command") in command and len(arguments) == 4:
-            return commands.get(command)(arguments.get("column"), arguments.get("value"), arguments.get("id"))
-    return f"Wrong command."
+from src.models import Discipline, Group, Grade, Student, Teacher
+from src.db import session
 
 
 def main():
-    print(handler_commands())
+
+    actions = ["create", "list", "update", "remove"]
+
+    models_create = {
+        "Group": create_group,
+        "Teacher": create_teacher,
+        "Student": create_student,
+        "Discipline": create_discipline,
+        "Grade": create_grade
+    }
+
+    models_update = {
+        "Group": update_group,
+        "Teacher": update_teacher,
+        "Student": update_student,
+        "Discipline": update_discipline,
+        "Grade": update_grade
+    }
+
+    arg = arg_parser()
+
+    if arg.action in "create":
+        if arg.model in models_create.keys():
+            result = models_create.get(arg.model)(arg)
+            return result
+
+    elif arg.action in "update":
+        if arg.model in models_update.keys():
+            result = models_update.get(arg.model)(arg)
+            return result
+
+
+def create_group(args):
+    result = Group(name=args.name)
+    session.add(result)
+    session.commit()
+    return f"Group {args.name} added."
+
+
+def create_teacher(args):
+    result = Teacher(fullname=args.name)
+    session.add(result)
+    session.commit()
+    return f"Teacher {args.name} added."
+
+
+def create_student(args):
+    result = Student(fullname=args.name, group_id=args.group_id)
+    session.add(result)
+    session.commit()
+    return f"Student {args.name} , group_id {args.group_id} added."
+
+
+def create_discipline(args):
+    result = Discipline(name=args.name, teacher_id=args.teacher_id)
+    session.add(result)
+    session.commit()
+    return f"Discipline {args.name}, teacher_id {args.teacher_id} added."
+
+
+def create_grade(args):
+    result = Grade(grade=args.grade, date_of=args.date_of, student_id=args.student_id, discipline_id=args.discipline_id)
+    session.add(result)
+    session.commit()
+    return f"Grade {args.grade}, date_of {args.date_of}, date_of {args.student_id}, date_of {args.discipline_id}, added."
+
+
+def update_group(args):
+    try:
+        group = session.query(Group).get(args.group_id)
+        group.name = args.name
+    except Exception as er:
+        return f"Failed: {er}"
+
+    session.add(group)
+    session.commit()
+
+    return f"Group {args.group_id}, update {args.name}."
+
+
+def update_teacher(args):
+    try:
+        teacher = session.query(Teacher).get(args.teacher_id)
+        teacher.name = args.fullname
+    except Exception as er:
+        return f"Failed: {er}"
+
+    session.add(teacher)
+    session.commit()
+
+    return f"Teacher {args.teacher_id}, update {args.fullname}."
+
+
+def update_student(args):
+    try:
+        student = session.query(Student).get(args.student_id)
+        student.name = args.fullname
+    except Exception as er:
+        return f"Failed: {er}"
+
+    session.add(student)
+    session.commit()
+
+    return f"Student {args.student_id}, update {args.fullname}."
+
+
+def update_discipline(args):
+    try:
+        discipline = session.query(Discipline).get(args.discipline_id)
+        discipline.name = args.name
+    except Exception as er:
+        return f"Failed: {er}"
+
+    session.add(discipline)
+    session.commit()
+
+    return f"Discipline {args.discipline_id}, update {args.name}."
+
+
+def update_grade(args):
+    try:
+        grade = session.query(Grade).get(args.grade_id)
+        grade.grade = args.grade
+        grade.date_of = args.date_of
+    except Exception as er:
+        return f"Failed: {er}"
+
+    session.add(grade)
+    session.commit()
+
+    return f"Grade {args.grade_id}, update {args.grade} and date_of {args.date_of}."
 
 
 if __name__ == "__main__":
-    main()
+    print(main())
